7b7d9fa367edbdb95be14ba76eebb4f191678037
Sooahn Shin <sooahnshin@g.harvard.edu>
2023-12-31 15:46:39 -0500

    Initial commit

---
+++ R/issueipe-package.R
@@ -0,0 +1,16 @@
+#' The 'issueipe' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name issueipe-package
+#' @aliases issueipe
+#' @useDynLib issueipe, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (NA). RStan: the R interface to Stan. R package version 2.32.3. https://mc-stan.org
+#'
+NULL
